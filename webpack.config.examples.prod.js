d2fcf454c08e4f45f5ead95d51f490a64dbb53aa
Thibault Friedrich <tf@elqui.fr>
2018-08-04 13:44:28 +0200

    update config file

--- webpack.config.examples.prod.js
+++ webpack.config.examples.prod.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'examples/index.jsx'),
   output: {
     publicPath: '/',
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, '.'),
   },
   module: {
     rules: [
